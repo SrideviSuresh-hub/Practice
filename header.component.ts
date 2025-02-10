23a94a99e52404a36e034b8adca61fcb5a08a681
SrideviSuresh-hub <Sridevi.Suresh@itorizon.com>
2025-02-10 23:14:57 +0530

    Add files via upload

---
+++ header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-header',
+    // template:'<h3>practice</h3>',
+    templateUrl:'./header.component.html',
+    standalone: false
+    
+})
+export class HeaderComponent{
+
+}
