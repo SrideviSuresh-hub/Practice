5d583573d72328575f520998933157144fcad7d5
SrideviSuresh-hub <Sridevi.Suresh@itorizon.com>
2025-02-12 08:34:55 +0530

    Add files via upload

--- header.component.ts
+++ header.component.ts
@@ -4,6 +4,8 @@ import { Component } from "@angular/core";
     selector:'app-header',
     // template:'<h3>practice</h3>',
     templateUrl:'./header.component.html',
+    // styleUrl:'./header.component.css',
+    styleUrls:['./header.component.css'],
     standalone: false
     
 })
